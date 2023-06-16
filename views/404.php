b8d7c363a46141023a956c13d19967846746bf7a
wils <tsikhaviwils@gmail.com>
2023-06-16 19:33:22 +0300

    update

--- views/404.php
+++ views/404.php
@@ -2,31 +2,22 @@
 <?php require('partials/nav.php') ?>
 
 <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <h1 class="text-2xl font-bold">Sorry. Page Not Found.</h1>
-
-        <p class="mt-4">
-            <a href="/" class="text-blue-500 underline">Go back home.</a>
-        </p>
-    </div>
     
-    <div class="flex items-center flex-col justify-center lg:flex-row py-28 px-6 md:px-24 md:py-20 lg:py-32 gap-16 lg:gap-28">
+    <div class="flex items-center mt-0 flex-col justify-center lg:flex-row py-10 px-6 md:px-24 md:py-10 lg:py-10 gap-16 lg:gap-28">
             <div class="w-full lg:w-1/2">
-                <img class="" src="assets/images/logo/sautis-website-favicon-white.svg" />
-                <img class="" src="assets/images/logo/sautis-high-resolution-logo-black-on-white-background.png" />
-                <img class="" src="" />
-                <a class="text-blue-500 underline" href="https://storyset.com/web">Web illustrations by Storyset</a>
+                <img class="" src="assets/images/404c.png" alt="404-error-with-a-cute-animal"/>
             </div>
             <div class="w-full lg:w-1/2">
                 <h1 class="py-4 text-3xl lg:text-4xl font-extrabold text-gray-800 dark:text-white">Looks like you've found the doorway to the great nothing</h1>
                 <p class="py-4 text-base text-gray-800 dark:text-white">The content you’re looking for doesn’t exist. Either it was removed, or you mistyped the link.</p>
                 <p class="py-2 text-base text-gray-800 dark:text-white">Sorry about that! Please visit our hompage to get where you need to go.</p>
-                <a href="/" class="text-blue-500 underline">
-                <button class="w-full lg:w-auto my-4 rounded-md px-1 sm:px-16 py-5 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Go back to Homepage</button>
+                <a href="/">
+                <button class="w-full lg:w-auto my-4 rounded-md px-1 sm:px-16 py-5 bg-red-600 text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-opacity-50">Go back to Homepage</button>
                 </a>
             </div>
+            
         </div>
-    
+        <a class="text-gray-500 underline text-sm" href="https://storyset.com/web" alt="Attribute to storyset.com, where the image for 404 was found">Web illustrations by Storyset</a>
 </main>
 
 <?php require('partials/footer.php') ?>
